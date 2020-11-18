9682c18a4ea0163ca0c338fe6c8818620460e871
Quang NV <quangnv1311@gmail.com>
2020-11-18 16:29:31 +0700

    add role

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -10,19 +10,22 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/shared/decorators/roles.decorator';
+import { RolesGuard } from 'src/shared/guards/roles.guard';
 import { IHttpResult } from 'src/shared/http-result';
 import { UserDto } from 'src/shared/models/user';
 import { ParseJsonPipe } from 'src/shared/pipes/json-parse.pipe';
 import { UserService } from './user.service';
 
 @Controller('user')
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(AuthGuard('jwt'), RolesGuard)
 export class UserController {
   /**
    *
    */
   constructor(private userService: UserService) {}
   @Get('get-all')
+  @Roles('admin')
   async getAllUser(
     @Query('filter', ParseJsonPipe) filter: UserDto,
     @Query('sort', ParseJsonPipe) sort,
@@ -33,16 +36,19 @@ export class UserController {
   }
 
   @Post('create')
+  @Roles('admin')
   async createUser(@Body() userDto: UserDto): Promise<IHttpResult> {
     return await this.userService.createUser(userDto);
   }
 
   @Put('update')
+  @Roles('admin')
   async updateUser(@Body() userDto: UserDto): Promise<IHttpResult> {
     return await this.userService.updateUser(userDto);
   }
 
   @Delete('delete')
+  @Roles('admin')
   async deleteUser(userId: string): Promise<IHttpResult> {
     return await this.userService.deleteUser(userId);
   }
